d58db79305f4826ae745b13523ef94244f82ae43
zqsd <17475349+zqsd1@users.noreply.github.com>
2021-02-19 20:30:51 +0100

    collision avec une brique

--- balle.js
+++ balle.js
@@ -4,14 +4,14 @@ var balle = {
     radius: 10,
     couleur: "purple",
     direction: "down",
-    vitesseX: 5,
-    vitesseY: 7
+    vitesseX: 2,
+    vitesseY: 0
 
 
 }
 
 var briques = [
-    new Brique(20, 0)
+    new Brique(100,200)
 ]
 
 function Brique(posX, posY) {
@@ -58,16 +58,39 @@ function bouger() {
     draw()
     balle.posX += balle.vitesseX;
     balle.posY += balle.vitesseY;
+    collision();
+    
+
+    window.requestAnimationFrame(bouger);
+
+}
+
+function collision(params) {
     if (balle.posX+balle.radius >= document.getElementById("canvas").width
      ||balle.posX-balle.radius <=0) {
         balle.vitesseX = -balle.vitesseX
     }
     if (balle.posY + balle.radius >= document.getElementById("canvas").height 
     || balle.posY - balle.radius <=0) {
-        balle.vitesseY = -balle.vitesseY;
-        
+        balle.vitesseY = -balle.vitesseY;        
     }
 
-    window.requestAnimationFrame(bouger);
+    //si y'a une value de la balle qui est dans une value de la brique 
+    if (balle.posX>=briques[0].posX &&balle.posX<=briques[0].posX+briques[0].width &&
+        balle.posY>=briques[0].posY &&balle.posY<=briques[0].posY+briques[0].height) {
+
+            //TODO un % sa devrait pouvoir le faire pour le requestAnimationFrame
+            //si la distance y de la balle par rapport à l'origine y de la brique est de 0 ou la hauteur de la brique (avec erreur lié à la vitesse de la balle)
+            if ((balle.posY - briques[0].posY <=briques[0].height)&&(balle.posY - briques[0].posY)>=briques[0].height+balle.vitesseY ||(balle.posY - briques[0].posY >=0)&&(balle.posY - briques[0].posY)<=balle.vitesseY) {
+                balle.vitesseY = -balle.vitesseY
+            }
+
+            //si les coord x de la balle - les coord x de la brique comprise entre 0 et la largeur de la brique avec marge erreur de la vitesse de la balle
+            if ((balle.posX - briques[0].posX <=briques[0].width)&&(balle.posX - briques[0].posX >=briques[0].width+balle.vitesseX)||(balle.posX - briques[0].posX >=0)&&(balle.posX - briques[0].posX <=balle.vitesseX)) {
+                balle.vitesseX = -balle.vitesseX;
+                console.log(balle.posX - briques[0].posX);
+            }
+        
+    }
 
 }
