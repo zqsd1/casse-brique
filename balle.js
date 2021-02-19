8f8e962dcbc008959b78bf91f73356bd80eef167
zqsd <17475349+zqsd1@users.noreply.github.com>
2021-02-19 18:41:14 +0100

    utilisation de la vitesse X et Y plutot qu'une direction comme sur MDN

--- balle.js
+++ balle.js
@@ -1,76 +1,45 @@
 var balle = {
-    posX : 100,
-    posY : 100,
-    radius:50,
-    couleur : "purple",
-    direction : "down",
-    
+    posX: 100,
+    posY: 100,
+    radius: 10,
+    couleur: "purple",
+    direction: "down",
+    vitesseX: 5,
+    vitesseY: 7
+
 
 }
 
-function draw(balle) {
+function draw() {
     var canvas = document.getElementById("canvas");
     if (canvas.getContext) {
-        var ctx= canvas.getContext("2d");
+        var ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         ctx.beginPath();
-        ctx.arc(balle.posX,balle.posY,balle.radius,0,Math.PI*2,true);
-        ctx.fillStyle= balle.couleur;
-        ctx.fill();      
-        
+        ctx.arc(balle.posX, balle.posY, balle.radius, 0, Math.PI * 2, true);
+        ctx.fillStyle = balle.couleur;
+        ctx.fill();
+
     }
 
 }
 
 draw(balle);
+window.requestAnimationFrame(bouger);
+//window.setInterval(bouger, 200, balle);
+function bouger() {
+    draw()
+    balle.posX += balle.vitesseX;
+    balle.posY += balle.vitesseY;
+    if (balle.posX+balle.radius >= document.getElementById("canvas").width
+     ||balle.posX-balle.radius <=0) {
+        balle.vitesseX = -balle.vitesseX
+    }
+    if (balle.posY + balle.radius >= document.getElementById("canvas").height 
+    || balle.posY - balle.radius <=0) {
+        balle.vitesseY = -balle.vitesseY;
+    }
 
-window.setInterval(bouger,200,balle);
-function bouger(balle) {
-    draw(balle)
-    if (balle.direction =="left") {
-        if (balle.posX + balle.radius >= document.getElementById("canvas").width) {
-            balle.direction = "right";
-            balle.posX -= 10;
-            
-        }
-        else
-        {
-            balle.posX += 10;
-        }
-    }
-    
-    if (balle.direction =="right") {
-        if (balle.posX - balle.radius <= 0) {
-            balle.direction = "left";
-            balle.posX += 10;
-            
-        }
-        else
-        {
-            balle.posX -= 10;
-        }
-    }
-    if (balle.direction =="up") {
-        if (balle.posY - balle.radius <= 0) {
-            balle.direction = "down";
-            balle.posY += 10;
-            
-        }
-        else
-        {
-            balle.posY -= 10;
-        }
-    }
-    if (balle.direction =="down") {
-        if (balle.posY + balle.radius >= document.getElementById("canvas").height) {
-            balle.direction = "up";
-            balle.posY -= 10;
-            
-        }
-        else
-        {
-            balle.posY += 10;
-        }
-    }
+    window.requestAnimationFrame(bouger);
 
 }
