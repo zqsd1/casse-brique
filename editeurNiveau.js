f5f86de16cdb54f410d224a8c2a98c0f936c7809
zqsd <17475349+zqsd1@users.noreply.github.com>
2021-02-20 18:48:11 +0100

    sauvegarde des brique en JSON

--- editeurNiveau.js
+++ editeurNiveau.js
@@ -2,9 +2,17 @@ var canvas = document.getElementById("canvas");
 var y = canvas.height / 2;
 var x = canvas.width;
 var nbBriquesX = 15;
-var nbBriquesY =15;
-var largeurBrique = x/nbBriquesX;
-var hauteurBrique = y/nbBriquesY;
+var nbBriquesY = 15;
+var largeurBrique = x / nbBriquesX;
+var hauteurBrique = y / nbBriquesY;
+
+var briques = [];
+
+function Brique(posX, posY) {
+
+    this.posX = posX;
+    this.posY = posY;
+}
 
 
 if (canvas.getContext) {
@@ -19,45 +27,45 @@ if (canvas.getContext) {
         }
     }
     ctx.stroke();
-    // ctx.beginPath();
-    // ctx.rect(50, 50, 10, 10);
-    // ctx.fillStyle = 'red';
-    // ctx.lineWidth = '2'
-    // ctx.stroke();
-    // ctx.fill();
-    // ctx.strokeRect(50, 50, 10, 10)
 }
+
 canvas.addEventListener("mousedown", clickMouse)
 
 function clickMouse(params) {
-    
+
 
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
         //recup info du decalage du canvas dans la page
         var rect = canvas.getBoundingClientRect();
         //coordonné du clic
-        var clicX = params.clientX-rect.x
-        var clicY = params.clientY-rect.y;
+        var clicX = params.clientX - rect.x
+        var clicY = params.clientY - rect.y;
         //le numero de la brique clicqué    
-        var positionXBrique = Math.trunc(clicX/largeurBrique);
-        var positionYBrique = Math.trunc(clicY/hauteurBrique);
-        ctx.fillRect(positionXBrique*largeurBrique,positionYBrique*hauteurBrique,largeurBrique,hauteurBrique);
+        var positionXBrique = Math.trunc(clicX / largeurBrique);
+        var positionYBrique = Math.trunc(clicY / hauteurBrique);
+        ctx.fillRect(positionXBrique * largeurBrique, positionYBrique * hauteurBrique, largeurBrique, hauteurBrique);
 
-         
-        // console.log(params.clientX - rect.x,params.offsetX, params.clientY - rect.y);
-        // console.log((params.clientX-rect.x)/(x/nbBriquesX))
-        // console.log((params.clientY-rect.y)/(y/nbBriquesY));
-        // ctx.fillStyle = "rgba(200,0,0,0.5)";
-        // ctx.fillRect(Math.trunc((params.clientX-rect.x)/(x/nbBriquesX))*x/nbBriquesX,Math.trunc((params.clientY-rect.y)/(y/nbBriquesY))*y/nbBriquesY,x / nbBriquesX, y / nbBriquesY)
+        briques.push(new Brique(positionXBrique, positionYBrique));
 
-
-
-
+        //converti le tableau pour que ça soit lisible en html
+        //TODO 1seul par position
+        //TODO effacer brique clic droit ou reclic gauche ?
+        var tmp = JSON.stringify(briques);
+        document.getElementById("briquesOutput").innerHTML = tmp;
+      
 
 
-        // ctx.fillStyle = "red";
+
+
+
         // //firefox a des prob pour la position du clic par rapport a la page et le decalage du canvas
+        //offset recup la position dans l'element choisi
+        // client meme chose pour l'element window
+        //page a partir du document
+        
+        //mouse event attribut : https://www.w3schools.com/jsref/obj_mouseevent.asp
+        
         // //pas décallé
         // ctx.fillRect(params.offsetX, params.offsetY, 10, 10);
         // ctx.fillStyle = 'rgba(0,0,200,0.5)';
