900b86ab58d63b17210c5733a17f5006928bdd13
zqsd <17475349+zqsd1@users.noreply.github.com>
2021-02-24 17:16:32 +0100

    +de 1 brique :evidement y'a un bug quand elle touche 2 briques en meme temps

--- hitbox.js
+++ hitbox.js
@@ -25,32 +25,64 @@ function loop(timeStamp) {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.beginPath();
-    ctx.save();
-    ctx.rect(brique.x, brique.y, brique.w, brique.h);
-    ctx.fillStyle = brique.collide ? "orange" : "blue";
-    ctx.fill();
+
+
+    //ctx.beginPath();
+    // for (let i = 0; i < briques.length; i++) {
+    //     const brique = briques[i];
+    //     ctx.save();
+    //     ctx.rect(brique.x, brique.y, brique.w, brique.h);
+    //     ctx.fillStyle = brique.collide ? "orange" : "blue";
+    //     ctx.fill();
+    //     ctx.restore();        
+    // }
+    briques.forEach(brique => {
+        ctx.beginPath();
+        ctx.save();
+        ctx.rect(brique.x, brique.y, brique.w, brique.h);
+        ctx.fillStyle = brique.collide ? "orange" : "blue";
+        ctx.fill();
+        ctx.restore();
+    });
+
 
     ctx.beginPath();
-    ctx.restore();
+    //ctx.restore();
     ctx.arc(balle.x, balle.y, balle.r, 0, Math.PI * 2);
     ctx.fill();
 
 }
 
-var brique = {
-    x: 150,
-    y: 150,
-    w: 100,
-    h: 150,
-    collide: false
-}
+// var brique = {
+//     x: 200,
+//     y: 100,
+//     w: 100,
+//     h: 150,
+//     collide: false
+// }
 
+var briques = [
+    brique1 = {
+        x: 100,
+        y: 100,
+        w: 100,
+        h: 100,
+        collide: false
+
+    },
+    brique2 = {
+        x: 200,
+        y: 100,
+        w: 100,
+        h: 100,
+        collide: false
+    }
+];
 var balle = {
     r: 10,
-    x: 60,
-    y: 100,
-    vx: 200,
+    x: 120,
+    y: 10,
+    vx: 100,
     vy: 100,
     mouvement: function () {
 
@@ -72,60 +104,65 @@ var balle = {
 
 
 function collision() {
-    //brique.collide = false
-    var testX = balle.x;
-    var testY = balle.y;
 
-    if (balle.x < brique.x)
-        //viens de la gauche
-        testX = brique.x;
-    else if (balle.x > brique.x + brique.w)
-        //vien de la droite
-        testX = brique.x + brique.w;
+    for (let i = 0; i < briques.length; i++) {
+        const brique = briques[i];
 
-    if (balle.y < brique.y)
-        //viens du haut 
-        testY = brique.y;
-    else if (balle.y > brique.y + brique.h)
-        //viens du bas
-        testY = brique.y + brique.h
 
-    //0 veut dire que c'est dedans
-    var distX = balle.x - testX;// +  rrive de la droite - arrive de la gauche
-    var distY = balle.y - testY;//+ arrive du bas - arrive du haut
+        //brique.collide = false
+        var testX = balle.x;
+        var testY = balle.y;
 
-    var distance = Math.sqrt((distX * distX) + (distY * distY));
-    if (distance <= balle.r) {
-        if (!brique.collide) {
+        if (balle.x < brique.x)
+            //viens de la gauche
+            testX = brique.x;
+        else if (balle.x > brique.x + brique.w)
+            //vien de la droite
+            testX = brique.x + brique.w;
 
-            if (distY == 0 || distX == 0) {
-                if (distY == 0) {
-                    balle.vx = -balle.vx;
-                }
-                if (distX == 0) {
-                    balle.vy = -balle.vy
-                }
-            }
-            else {
-                if ((distX * distX) > (distY * distY)) {
-                    balle.vx = -balle.vx
-                }
-                else if ((distX * distX) < (distY * distY)) {
-                    balle.vy = -balle.vy
+        if (balle.y < brique.y)
+            //viens du haut 
+            testY = brique.y;
+        else if (balle.y > brique.y + brique.h)
+            //viens du bas
+            testY = brique.y + brique.h
+
+        //0 veut dire que c'est dedans
+        var distX = balle.x - testX;// +  rrive de la droite - arrive de la gauche
+        var distY = balle.y - testY;//+ arrive du bas - arrive du haut
+
+        var distance = Math.sqrt((distX * distX) + (distY * distY));
+        if (distance <= balle.r) {
+            if (!brique.collide) {
+
+                if (distY == 0 || distX == 0) {
+                    if (distY == 0) {
+                        balle.vx = -balle.vx;
+                    }
+                    if (distX == 0) {
+                        balle.vy = -balle.vy
+                    }
                 }
                 else {
-                    balle.vx = -balle.vx
-                    balle.vy = -balle.vy
+                    if ((distX * distX) > (distY * distY)) {
+                        balle.vx = -balle.vx
+                    }
+                    else if ((distX * distX) < (distY * distY)) {
+                        balle.vy = -balle.vy
+                    }
+                    else {
+                        balle.vx = -balle.vx
+                        balle.vy = -balle.vy
+                    }
+
                 }
-
             }
+
+            brique.collide = true;
+
         }
-
-        brique.collide = true;
-
+        else {
+            brique.collide = false;
+        }
     }
-    else {
-        brique.collide = false;
-    }
-
 }
