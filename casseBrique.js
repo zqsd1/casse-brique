10c47f881610f851e26294ab521ae6d3e9fa02e9
zqsd <17475349+zqsd1@users.noreply.github.com>
2021-03-26 17:22:21 +0100

    du menage, des constante, une classe Brique

--- casseBrique.js
+++ casseBrique.js
@@ -1,18 +1,53 @@
+
+const BRIQUE_X = 15// nb colonne brique
+const BRIQUE_Y = 15//nb ligne de brique
+const RATIO_X = 1 // le % despace x occupe par les brique
+const RATIO_Y = .5 //le % du canvas occupé par la zone de brique
+
+const BALLE_SPEED_X = 0
+const BALLE_SPEED_Y = 200
+
+const BRIQUE_PV_COLOR = {
+    1: "red",
+    2: "yellow",
+    3: "green"
+}
+const NIVEAU1 = [{ "x": 8, "y": 1 }, { "x": 6, "y": 1 }, { "x": 6, "y": 3 }, { "x": 8, "y": 3 }, { "x": 10, "y": 3 }, { "x": 10, "y": 1 }, { "x": 8, "y": 6 }, { "x": 4, "y": 8 }, { "x": 1, "y": 6 }, { "x": 1, "y": 12 }, { "x": 10, "y": 10 }, { "x": 12, "y": 10 }, { "x": 13, "y": 6 }, { "x": 13, "y": 2 }]
+const NIVEAU2 =
+    [{ "x": 1, "y": 3 }, { "x": 2, "y": 3 }, { "x": 3, "y": 3 }, { "x": 4, "y": 3 }, { "x": 5, "y": 3 }, { "x": 6, "y": 3 }, { "x": 7, "y": 3 }, { "x": 8, "y": 3 }, { "x": 9, "y": 3 }, { "x": 10, "y": 3 }, { "x": 11, "y": 3 }, { "x": 11, "y": 4 }, { "x": 11, "y": 5 }, { "x": 11, "y": 6 }, { "x": 11, "y": 7 }, { "x": 11, "y": 8 }, { "x": 10, "y": 8 }, { "x": 9, "y": 8 }, { "x": 6, "y": 8 }, { "x": 4, "y": 8 }, { "x": 5, "y": 8 }, { "x": 2, "y": 8 }, { "x": 1, "y": 8 }]
+const NIVEAU3 = [{ "x": 3, "y": 3 }, { "x": 4, "y": 3 }, { "x": 5, "y": 3 }, { "x": 6, "y": 3 }, { "x": 7, "y": 3 }, { "x": 8, "y": 3 }, { "x": 9, "y": 3 }, { "x": 10, "y": 3 }, { "x": 10, "y": 4 }, { "x": 10, "y": 5 }, { "x": 10, "y": 6 }, { "x": 10, "y": 7 }, { "x": 9, "y": 7 }, { "x": 8, "y": 7 }, { "x": 7, "y": 7 }, { "x": 6, "y": 7 }, { "x": 5, "y": 7 }, { "x": 4, "y": 7 }, { "x": 3, "y": 7 }, { "x": 3, "y": 5 }, { "x": 3, "y": 6 }, { "x": 3, "y": 4 }, { "x": 3, "y": 8 }, { "x": 4, "y": 8 }, { "x": 5, "y": 8 }, { "x": 10, "y": 8 }, { "x": 10, "y": 9 }, { "x": 10, "y": 10 }, { "x": 9, "y": 10 }, { "x": 8, "y": 10 }, { "x": 7, "y": 10 }, { "x": 6, "y": 10 }, { "x": 5, "y": 10 }, { "x": 4, "y": 10 }, { "x": 3, "y": 10 }, { "x": 3, "y": 9 }, { "x": 5, "y": 9 }, { "x": 4, "y": 9 }]
+
+const NIVEAU4 = [{ "posX": 0, "posY": 5, "niveau": 1 }, { "posX": 1, "posY": 5, "niveau": 1 }, { "posX": 2, "posY": 5, "niveau": 1 }, { "posX": 3, "posY": 5, "niveau": 1 }, { "posX": 5, "posY": 5, "niveau": 1 }, { "posX": 4, "posY": 5, "niveau": 1 }, { "posX": 6, "posY": 5, "niveau": 1 }, { "posX": 7, "posY": 5, "niveau": 1 }, { "posX": 8, "posY": 5, "niveau": 1 }, { "posX": 9, "posY": 5, "niveau": 1 }, { "posX": 10, "posY": 5, "niveau": 1 }, { "posX": 11, "posY": 5, "niveau": 1 }, { "posX": 12, "posY": 5, "niveau": 1 }, { "posX": 13, "posY": 5, "niveau": 1 }, { "posX": 14, "posY": 5, "niveau": 1 }, { "posX": 13, "posY": 6, "niveau": 3 }, { "posX": 14, "posY": 6, "niveau": 3 }, { "posX": 12, "posY": 6, "niveau": 3 }, { "posX": 11, "posY": 6, "niveau": 3 }, { "posX": 10, "posY": 6, "niveau": 3 }, { "posX": 9, "posY": 6, "niveau": 3 }, { "posX": 8, "posY": 6, "niveau": 3 }, { "posX": 7, "posY": 6, "niveau": 3 }, { "posX": 6, "posY": 6, "niveau": 3 }, { "posX": 5, "posY": 6, "niveau": 3 }, { "posX": 4, "posY": 6, "niveau": 3 }, { "posX": 3, "posY": 6, "niveau": 3 }, { "posX": 2, "posY": 6, "niveau": 3 }, { "posX": 1, "posY": 6, "niveau": 3 }, { "posX": 0, "posY": 6, "niveau": 3 }, { "posX": 7, "posY": 7, "niveau": 1 }, { "posX": 7, "posY": 8, "niveau": 1 }, { "posX": 7, "posY": 9, "niveau": 1 }, { "posX": 7, "posY": 10, "niveau": 1 }, { "posX": 7, "posY": 11, "niveau": 1 }, { "posX": 13, "posY": 4, "niveau": 2 }, { "posX": 1, "posY": 4, "niveau": 2 }, { "posX": 3, "posY": 3, "niveau": 2 }, { "posX": 5, "posY": 4, "niveau": 2 }, { "posX": 7, "posY": 3, "niveau": 2 }, { "posX": 9, "posY": 4, "niveau": 2 }, { "posX": 11, "posY": 3, "niveau": 2 }, { "posX": 8, "posY": 7, "niveau": 2 }, { "posX": 6, "posY": 7, "niveau": 2 }, { "posX": 5, "posY": 8, "niveau": 2 }, { "posX": 4, "posY": 7, "niveau": 2 }, { "posX": 9, "posY": 8, "niveau": 2 }, { "posX": 10, "posY": 7, "niveau": 2 }, { "posX": 11, "posY": 8, "niveau": 2 }, { "posX": 12, "posY": 7, "niveau": 2 }, { "posX": 3, "posY": 8, "niveau": 2 }, { "posX": 2, "posY": 7, "niveau": 2 }, { "posX": 14, "posY": 11, "niveau": 1 }, { "posX": 11, "posY": 11, "niveau": 1 }, { "posX": 4, "posY": 11, "niveau": 1 }, { "posX": 0, "posY": 11, "niveau": 1 }]
+
+const NIVEAU_TEST = [
+    {
+        "posX": 0,
+        "posY": 0,
+        "niveau": 1
+    },
+    {
+        "posX": 0,
+        "posY": 15,
+        "niveau": 2
+    },
+    {
+        "posX": 10,
+        "posY": 0,
+        "niveau": 3
+    },
+]
 var canvas;
 var ctx;
 var secondsPassed = 0;
 var oldTimeStamp = 0;
-var nbBriqueX = 15;
-var nbBriqueY = 30
+
 var briques = [
 
 ];
 
 
 
-document.onload = init()
-
-function init() {
+(function init() {
 
     canvas = document.getElementById("canvas");
     if (canvas.getContext) {
@@ -20,7 +55,7 @@ function init() {
 
     }
     window.requestAnimationFrame(loop)
-}
+})();
 
 
 var barre = {
@@ -39,16 +74,19 @@ var barre = {
             isDirDoite ? this.x += this.vx * secondsPassed : this.x -= this.vx * secondsPassed;
         }
     },
-    draw: function (params) {
+    draw: function () {
         ctx.beginPath();
+
+        ctx.save();
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.restore();
     }
 }
 
 var balle = {
     r: 10,
-    x: 250,
+    x: 30,
     y: 760,
     vx: 0,
     vy: 0,
@@ -59,11 +97,14 @@ var balle = {
     isVxReversed: false,
     isVyReversed: false,
 
-    draw: function (params) {
+    draw: function () {
         ctx.beginPath();
+
+        ctx.save();
         ctx.fillStyle = "black";
         ctx.arc(balle.x, balle.y, balle.r, 0, Math.PI * 2);
         ctx.fill();
+        ctx.restore()
     },
     mouvement: function () {
 
@@ -75,13 +116,12 @@ var balle = {
 
         if (this.x > canvas.width || this.x < 0) {
             this.inverserVx();
-            //this.vx = -this.vx;
 
         }
 
         if (this.y > canvas.height || this.y < 0) {
             this.inverserVy();
-            //this.vy = -this.vy;
+
         }
     },
     inverserVx: function () {
@@ -135,7 +175,9 @@ var balle = {
         var distance = Math.sqrt((distX * distX) + (distY * distY));
         //collision détecté
         if (distance <= this.r) {
+            console.log("call callback")
             ifCollisionCallBack(distX, distY);
+            console.log("apres callback");
             return true;
 
         }
@@ -163,29 +205,46 @@ var balle = {
             balle.inverserVx();
             balle.inverserVy();
         }
+        console.log("callback fait");
 
     }
 
 
 }
+/** */
+function Brique(x, y, pv) {
 
-/**
- * des briques a charger ;
- */
-const lvl4 = [{ "posX": 0, "posY": 5, "niveau": "lvl1" }, { "posX": 1, "posY": 5, "niveau": "lvl1" }, { "posX": 2, "posY": 5, "niveau": "lvl1" }, { "posX": 3, "posY": 5, "niveau": "lvl1" }, { "posX": 5, "posY": 5, "niveau": "lvl1" }, { "posX": 4, "posY": 5, "niveau": "lvl1" }, { "posX": 6, "posY": 5, "niveau": "lvl1" }, { "posX": 7, "posY": 5, "niveau": "lvl1" }, { "posX": 8, "posY": 5, "niveau": "lvl1" }, { "posX": 9, "posY": 5, "niveau": "lvl1" }, { "posX": 10, "posY": 5, "niveau": "lvl1" }, { "posX": 11, "posY": 5, "niveau": "lvl1" }, { "posX": 12, "posY": 5, "niveau": "lvl1" }, { "posX": 13, "posY": 5, "niveau": "lvl1" }, { "posX": 14, "posY": 5, "niveau": "lvl1" }, { "posX": 13, "posY": 6, "niveau": "lvl3" }, { "posX": 14, "posY": 6, "niveau": "lvl3" }, { "posX": 12, "posY": 6, "niveau": "lvl3" }, { "posX": 11, "posY": 6, "niveau": "lvl3" }, { "posX": 10, "posY": 6, "niveau": "lvl3" }, { "posX": 9, "posY": 6, "niveau": "lvl3" }, { "posX": 8, "posY": 6, "niveau": "lvl3" }, { "posX": 7, "posY": 6, "niveau": "lvl3" }, { "posX": 6, "posY": 6, "niveau": "lvl3" }, { "posX": 5, "posY": 6, "niveau": "lvl3" }, { "posX": 4, "posY": 6, "niveau": "lvl3" }, { "posX": 3, "posY": 6, "niveau": "lvl3" }, { "posX": 2, "posY": 6, "niveau": "lvl3" }, { "posX": 1, "posY": 6, "niveau": "lvl3" }, { "posX": 0, "posY": 6, "niveau": "lvl3" }, { "posX": 7, "posY": 7, "niveau": "lvl1" }, { "posX": 7, "posY": 8, "niveau": "lvl1" }, { "posX": 7, "posY": 9, "niveau": "lvl1" }, { "posX": 7, "posY": 10, "niveau": "lvl1" }, { "posX": 7, "posY": 11, "niveau": "lvl1" }, { "posX": 13, "posY": 4, "niveau": "lvl2" }, { "posX": 1, "posY": 4, "niveau": "lvl2" }, { "posX": 3, "posY": 3, "niveau": "lvl2" }, { "posX": 5, "posY": 4, "niveau": "lvl2" }, { "posX": 7, "posY": 3, "niveau": "lvl2" }, { "posX": 9, "posY": 4, "niveau": "lvl2" }, { "posX": 11, "posY": 3, "niveau": "lvl2" }, { "posX": 8, "posY": 7, "niveau": "lvl2" }, { "posX": 6, "posY": 7, "niveau": "lvl2" }, { "posX": 5, "posY": 8, "niveau": "lvl2" }, { "posX": 4, "posY": 7, "niveau": "lvl2" }, { "posX": 9, "posY": 8, "niveau": "lvl2" }, { "posX": 10, "posY": 7, "niveau": "lvl2" }, { "posX": 11, "posY": 8, "niveau": "lvl2" }, { "posX": 12, "posY": 7, "niveau": "lvl2" }, { "posX": 3, "posY": 8, "niveau": "lvl2" }, { "posX": 2, "posY": 7, "niveau": "lvl2" }, { "posX": 14, "posY": 11, "niveau": "lvl1" }, { "posX": 11, "posY": 11, "niveau": "lvl1" }, { "posX": 4, "posY": 11, "niveau": "lvl1" }, { "posX": 0, "posY": 11, "niveau": "lvl1" }]
-const lvl1 = [{ "x": 8, "y": 1 }, { "x": 6, "y": 1 }, { "x": 6, "y": 3 }, { "x": 8, "y": 3 }, { "x": 10, "y": 3 }, { "x": 10, "y": 1 }, { "x": 8, "y": 6 }, { "x": 4, "y": 8 }, { "x": 1, "y": 6 }, { "x": 1, "y": 12 }, { "x": 10, "y": 10 }, { "x": 12, "y": 10 }, { "x": 13, "y": 6 }, { "x": 13, "y": 2 }]
-const lvl2 =
-    [{ "x": 1, "y": 3 }, { "x": 2, "y": 3 }, { "x": 3, "y": 3 }, { "x": 4, "y": 3 }, { "x": 5, "y": 3 }, { "x": 6, "y": 3 }, { "x": 7, "y": 3 }, { "x": 8, "y": 3 }, { "x": 9, "y": 3 }, { "x": 10, "y": 3 }, { "x": 11, "y": 3 }, { "x": 11, "y": 4 }, { "x": 11, "y": 5 }, { "x": 11, "y": 6 }, { "x": 11, "y": 7 }, { "x": 11, "y": 8 }, { "x": 10, "y": 8 }, { "x": 9, "y": 8 }, { "x": 6, "y": 8 }, { "x": 4, "y": 8 }, { "x": 5, "y": 8 }, { "x": 2, "y": 8 }, { "x": 1, "y": 8 }]
-const lvl3 = [{ "x": 3, "y": 3 }, { "x": 4, "y": 3 }, { "x": 5, "y": 3 }, { "x": 6, "y": 3 }, { "x": 7, "y": 3 }, { "x": 8, "y": 3 }, { "x": 9, "y": 3 }, { "x": 10, "y": 3 }, { "x": 10, "y": 4 }, { "x": 10, "y": 5 }, { "x": 10, "y": 6 }, { "x": 10, "y": 7 }, { "x": 9, "y": 7 }, { "x": 8, "y": 7 }, { "x": 7, "y": 7 }, { "x": 6, "y": 7 }, { "x": 5, "y": 7 }, { "x": 4, "y": 7 }, { "x": 3, "y": 7 }, { "x": 3, "y": 5 }, { "x": 3, "y": 6 }, { "x": 3, "y": 4 }, { "x": 3, "y": 8 }, { "x": 4, "y": 8 }, { "x": 5, "y": 8 }, { "x": 10, "y": 8 }, { "x": 10, "y": 9 }, { "x": 10, "y": 10 }, { "x": 9, "y": 10 }, { "x": 8, "y": 10 }, { "x": 7, "y": 10 }, { "x": 6, "y": 10 }, { "x": 5, "y": 10 }, { "x": 4, "y": 10 }, { "x": 3, "y": 10 }, { "x": 3, "y": 9 }, { "x": 5, "y": 9 }, { "x": 4, "y": 9 }]
+    this.w = canvas.width / (BRIQUE_X/RATIO_X);
+    this.h = canvas.height / (BRIQUE_Y/RATIO_Y);
 
+    this.x = x * this.w
+    this.y = y * this.h;
+    this.pv = pv
 
-const config = {
-    "difficulte": {
-        "lvl1": "red",
-        "lvl2": "green",
-        "lvl3": "yellow"
+    this.draw = function (params) {
+        ctx.beginPath();
+
+        ctx.save();
+        ctx.rect(this.x, this.y, this.w, this.h);
+        ctx.fillStyle = BRIQUE_PV_COLOR[this.pv]
+        ctx.fill();
+        ctx.restore();
+    }
+
+    /**
+     * 
+     * @param {number} dmg les degats infligé
+     * @returns 
+     */
+    this.takeDamage = function (dmg = 1) {
+        this.pv = this.pv - dmg;
+        return this.pv;
     }
 }
+
+NIVEAU_TEST.forEach(element => {
+    briques.push(new Brique(element.posX, element.posY, element.niveau))
+})
 
 /**
  * fait dessiner le canvas avec une notion temporelle
@@ -215,12 +274,8 @@ function draw() {
 
     //les briques a dessiner
     briques.forEach(brique => {
-        ctx.beginPath();
-        ctx.save();
-        ctx.rect(brique.x, brique.y, brique.w, brique.h);
-        ctx.fillStyle = config.difficulte[brique.niveau];
-        ctx.fill();
-        ctx.restore();
+
+        brique.draw();
     });
 
     balle.draw();
@@ -230,17 +285,6 @@ function draw() {
 }
 
 
-lvl4.forEach(element => {
-    briques.push(
-        {
-            x: element.posX * canvas.width / nbBriqueX,
-            y: element.posY * canvas.height / nbBriqueY,
-            w: canvas.width / nbBriqueX,
-            h: canvas.height / nbBriqueY,
-            niveau: element.niveau
-        }
-    )
-})
 
 
 
@@ -250,44 +294,23 @@ lvl4.forEach(element => {
  */
 function collide(briques) {
 
-    //teste pour toute les briques
-    //for parce que je retire les brique qui ont une collision et avec foreach sa en saute apres en avoir retiré
+
     for (let index = 0; index < briques.length; index++) {
         const brique = briques[index];
-        const isCollision = balle.isCollisionWithBrique(brique, balle.changerDirectionAfterCollide);
 
-        //  const [distX, distY] = detectCollisionBetweenRectAndBall(brique, balle);
-        ///const isCollision = isCollisionBetweenRectAndBall(brique, balle, changerDirectionBalleAfterCollide);
-        // if (isCollision) {
-        //TODO promise ? async ?
-        //     damageBrique(brique);
-        // }
+        const touch = balle.isCollisionWithBrique(brique, balle.changerDirectionAfterCollide);
+        if (touch && brique instanceof Brique) {
 
+            if (brique.takeDamage() <= 0) {
 
-        if (isCollision) {
-            switch (brique.niveau) {
-                case "lvl1":
-                    briques.splice(index, 1);
-                    index--;
-                    break;
-                case "lvl2":
-                    brique.niveau = "lvl1";
-                    break;
-                case "lvl3":
-                    brique.niveau = "lvl2";
-                    break;
-
-                default:
-                    break;
+                briques.splice(index, 1);
+                index--;
             }
 
         }
-
-
-
     }
-}
 
+}
 
 /**
  * les event pour deplacer la barre
@@ -306,9 +329,6 @@ canvas.addEventListener('mousemove', e => {
 document.addEventListener("keydown", e => {
     if (e.key == "ArrowRight") {
 
-
-
-
         barre.mouvement(true);
     }
     if (e.key == "ArrowLeft") {
@@ -316,13 +336,28 @@ document.addEventListener("keydown", e => {
     }
     //space bar
     if (e.key == " ") {
-        balle.vx == 0 ? balle.vx = 100 : balle.vx = balle.vx;
-        balle.vy == 0 ? balle.vy = 200 : balle.vy = balle.vy;
+        balle.vx == 0 ? balle.vx = BALLE_SPEED_X : balle.vx = balle.vx;
+        balle.vy == 0 ? balle.vy = BALLE_SPEED_Y : balle.vy = balle.vy;
     }
 })
 
 
-        //////////////
+
+
+// var eventKillBrick = new CustomEvent("killBrick", { detail: this })
+
+
+// document.addEventListener("killBrick", e => {
+//     console.log(e);
+// })
+
+
+// document.dispatchEvent(eventKillBrick);
+
+
+
+
+//////////////
         // const SPEED = 2
         // let lastRenderTime = 0;
         // function main(time) {
